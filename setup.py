d9ca360f7213e7dc9d5dbf3d891eca029759a255
David Zabner <david.zabner@gmail.com>
2020-06-30 12:00:21 -0400

    updates to setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,17 @@ from distutils.core import setup
 setup(
   name = 'pylisp',
   packages = ['pylisp'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Basic lisp functions in python',   # Give a short description about your library
   author = 'David Zabner',                   # Type in your name
   author_email = 'david.zabner@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/nw0428/pylisp',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/nw0428/pylisp/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/nw0428/pylisp/archive/v_03.tar.gz',    # I explain this later on
   keywords = ['Lisp', 'Lists', 'LISP', 'car', 'cdr', 'cons'],   # Keywords that define your package best
-  install_requires=[],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Students',
+    'Intended Audience :: Education',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
