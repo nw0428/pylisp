fc4ea770cbde8a7f26a7147296e8375fd38bc4ce
David Zabner <david.zabner@gmail.com>
2020-06-30 11:45:51 -0400

    Initial commit

---
+++ pylisp/__init__.py
@@ -0,0 +1,8 @@
+from .pylisp import cons
+from .pylisp import car
+from .pylisp import cdr
+from .pylisp import is_null
+from .pylisp import is_pair
+from .pylisp import ls
+from .pylisp import null
+
---
+++ pylisp/pylisp.py
@@ -0,0 +1,51 @@
+def cons(x, y):
+    '''
+    cons(x, y) takes its two arguments and puts them into a python tuple.
+    This tuple can be used to represent a node in a linked list, or a pair
+    '''
+    return (x, y)
+
+def car(x):
+    '''
+    car(cons(x, y)) returns x. In other words, car returns the head of a linked list or the left item of a pair
+    '''
+    return x[0]
+
+def cdr(x):
+    '''
+    cdr(cons(x, y)) returns y. In other words, cdr returns the tail of a linked list or the right item of a pair
+    '''
+    return x[1]
+
+def is_null(x):
+    '''
+    is_null(null) returns true
+    is_null(()) returns true
+    In all other cases is_null returns false
+    '''
+    return x == ()
+
+def is_pair(x):
+    '''
+    is_pair(x) returns true if x is a pair/2-tuple/cons cell or null
+    if x is anything else it returns false
+    '''
+    return is_null(x) or (isinstance(x, tuple) and len(x) == 2)
+
+# null: Final = ()
+null = ()
+
+def ls(*argv):
+    '''
+    ls(*argv) takes a list of arguments and turns them into a lisp style list of cons cells
+    so ls(1,2,3,4,5) => cons(1, cons(2, cons(3, cons(4, cons(5, ())))))
+    '''
+    if len(argv) is 0:
+        return null
+    else:
+        return cons(argv[0], ls(*argv[1:]))
+
+
+if __name__ == "__main__":
+    print("Nothing in main at the moment")
+
