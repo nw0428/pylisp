882b05876bf2a2d98a2c4cbfa0bc0e80ca6a3d18
David Zabner <david.zabner@gmail.com>
2020-06-30 12:22:53 -0400

    small change to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from distutils.core import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+
 setup(
   name = 'pylispy',
   packages = ['pylispy'],   # Chose the same as "name"
-  version = '0.4',      # Start with a small number and increase it with every change you make
+  version = '0.4.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Basic lisp functions in python',   # Give a short description about your library
   author = 'David Zabner',                   # Type in your name
   author_email = 'david.zabner@gmail.com',      # Type in your E-Mail
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   url = 'https://github.com/nw0428/pylisp',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/nw0428/pylisp/archive/v_04.tar.gz',    # I explain this later on
   keywords = ['Lisp', 'Lists', 'LISP', 'car', 'cdr', 'cons'],   # Keywords that define your package best
