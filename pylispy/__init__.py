78e82bf9ddbb029e071851c8fe2d066c777cfd2e
David Zabner <david.zabner@gmail.com>
2020-06-30 22:50:11 -0400

    Re-added the pylispy files

---
+++ pylispy/__init__.py
@@ -0,0 +1,8 @@
+from .pylispy import cons
+from .pylispy import car
+from .pylispy import cdr
+from .pylispy import is_null
+from .pylispy import is_pair
+from .pylispy import ls
+from .pylispy import null
+
